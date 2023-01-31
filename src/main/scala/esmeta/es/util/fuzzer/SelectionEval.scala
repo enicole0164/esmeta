8518f6c068b3ecb47c380dc3ad4f1aa0b74541bd
Kanguk Lee <p51lee@kaist.ac.kr>
2023-01-31 16:59:38 +0900

    Add log

--- src/main/scala/esmeta/es/util/fuzzer/SelectionEval.scala
+++ src/main/scala/esmeta/es/util/fuzzer/SelectionEval.scala
@@ -32,17 +32,17 @@ object SelectionEval {
         println(s"index: $idx")
       }
       idx += 1
-      println(script.name)
       val cov = getCoverage(baseDir)
       val (_, updated, _, blockingSet) = cov.runAndCheckBlockings(script)
       if (updated) {
+        println(s"Updated: $name")
         numCoveredBugs += 1
       } else {
         val blockingIsBug = blockingSet.foldLeft(false) {
           case (isBug, blocking) =>
             if isBug then isBug
             else
-              println(s"Blocking: ${blocking.code}")
+              println(s"Blocking: ${blocking.code}, name: $name")
               targets.foldLeft(false) {
                 case (isTargetBug, target) =>
                   if isTargetBug then true
